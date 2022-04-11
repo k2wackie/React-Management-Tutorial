49adb082a142955077f83d30dfb74d52687f7f35
Kyle KIM <k2wackie@gmail.com>
2022-04-11 12:11:21 +0900

    Create Customers Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -6,33 +8,25 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const connection = mysql.createConnection({
+  host:conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-  res.send([
-    {
-      'id': 1,
-      'image': 'https://placeimg.com/64/64/1',
-      'name': '홍길동',
-      'birthday': '961222',
-      'gender': '남자',
-      'job': '학생',
-    },
-    {
-      'id': 2,
-      'image': 'https://placeimg.com/64/64/2',
-      'name': '홍길동',
-      'birthday': '840512',
-      'gender': '남자',
-      'job': '프로그래머',
-    },
-    {
-      'id': 3,
-      'image': 'https://placeimg.com/64/64/3',
-      'name': '홍길동',
-      'birthday': '950212',
-      'gender': '남자',
-      'job': '디자이너',
-    },
-  ])
+  connection.query(
+    "SELECT * FROM management.customer;",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
