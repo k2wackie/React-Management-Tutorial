bf97451efcf499eda58e772bbdd851e1c9e6f26f
Kyle KIM <k2wackie@gmail.com>
2022-04-11 09:59:48 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-  res.send({message: 'Hello Express!!'});
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '홍길동',
+      'birthday': '961222',
+      'gender': '남자',
+      'job': '학생',
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '홍길동',
+      'birthday': '840512',
+      'gender': '남자',
+      'job': '프로그래머',
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '홍길동',
+      'birthday': '950212',
+      'gender': '남자',
+      'job': '디자이너',
+    },
+  ])
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
