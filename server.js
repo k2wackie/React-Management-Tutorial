8c2e59db93985b2b6033fa5159033f444e5d2293
Kyle KIM <k2wackie@gmail.com>
2022-04-12 09:44:18 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const mysql = require('mysql');
+const multer = require('multer');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,6 +21,8 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+const upload = multer({ dest: './upload' });
+
 app.get('/api/customers', (req, res) => {
   connection.query(
     "SELECT * FROM management.customer;",
@@ -27,6 +30,29 @@ app.get('/api/customers', (req, res) => {
       res.send(rows);
     }
   );
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'INSERT INTO management.customer VALUES (null, ?, ?, ?, ?, ?)';
+  console.log(req.body);
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  console.log(image);
+  console.log(name);
+  console.log(birthday);
+  console.log(gender);
+  console.log(job);
+
+
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params, (err, rows, fields) => {
+    res.send(rows);
+  })
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
